f8f0c58a633a7ab21a76eae78ea7bb7790eec040
victory wagor <wagorvictory@gmail.com>
2026-03-02 18:31:01 +0100

    working http

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    if(req.url === '/') {
-        res.end('Welcome to our home page')
-    }
-    if(req.url === '/about') {
-        res.end('Here is our short history')
-    }
-    res.end(
-        `<h1>Oops!</h1>
-        <p>We can't seem to find the page you are looking for</p>
-        <a href="/">back home</a>
-        `
-    )
+    console.log('request event')
+    res.end('Hello World')
 })
 
-server.listen(5000)
+server.listen(5000, () => {
+    console.log('Server listening on port : 5000...')
+})
